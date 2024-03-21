b26d266d12f3d6a218c3b7580e847b0006b32046
Artur Rodrigues <arturhoo@gmail.com>
2024-03-21 09:12:34 +0000

    nit: fix formatting

--- terraform/gcp_iam.tf
+++ terraform/gcp_iam.tf
@@ -4,11 +4,9 @@ resource "google_service_account" "default" {
 }
 
 resource "google_storage_bucket_iam_binding" "viewer" {
-  bucket = var.gcs_bucket
-  role   = "roles/storage.objectViewer"
-  members = [
-    "serviceAccount:${google_service_account.default.email}",
-  ]
+  bucket  = var.gcs_bucket
+  role    = "roles/storage.objectViewer"
+  members = ["serviceAccount:${google_service_account.default.email}"]
 }
 
 locals {
